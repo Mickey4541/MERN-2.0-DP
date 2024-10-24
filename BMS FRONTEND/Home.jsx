91807215cbcda230641e2d38f8cdbee09c8b5ac5
mickey4541 <rajanbhandari4541@gmail.com>
2024-10-24 15:54:27 +0545

    learned components, props, routing

---
+++ BMS FRONTEND/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home page is on the way.</div>
+  )
+}
+
+export default Home
--- BMS FRONTEND/src/App.jsx
+++ BMS FRONTEND/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../Home'
+import About from './About'
 
 const App = () => {
   return (
-    <div>
-      <h1>Rajan Bhandari</h1>
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<h1>Hello contact Page</h1>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
